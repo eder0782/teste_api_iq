c6f8cf32d9d367ffd4d9a203cc83e0a6ed2d49f1
eder0782 <eder0782@gmail.com>
2020-08-18 22:45:53 -0400

    chamar api iq

---
+++ api_iq/api_iq.py
@@ -0,0 +1,54 @@
+#from app.models import tabelas
+from app.iqoptionapi.stable_api import IQ_Option
+import time
+from datetime import datetime
+
+API = None
+
+def conectar(email,senha):
+    global API
+    API = IQ_Option(email,senha)
+    API.connect()
+    retorno = None
+    while True:    
+        if API.check_connect() == False:
+                   		
+            API.connect()
+            retorno = False
+        else:
+           
+            retorno = True
+            break
+        
+        time.sleep(1)
+    
+    return retorno
+
+
+def alterar_conta(tipo_conta):
+    try:
+        global API
+        API.change_balance(tipo_conta)
+        return True
+    except :
+        return False
+    
+
+def pegar_balanco():
+    try:
+        return API.get_balance()
+    except :
+        return 'FALHA NA CONEXÃO'
+
+def desconectar():
+    try:
+        API.api.close()
+        return True
+    except :
+        return False
+    
+    
+    
+
+
+
--- chamar.py
+++ chamar.py
@@ -1,13 +1,28 @@
-import teste as t 
-import time
+from api_iq.app import api_iq as api_iq
 
-t.conectar()
-# time.sleep(10)
-# print(t.balanco())
-# t.muda_var(False)
-# time.sleep(1)
-# print(t.balanco())
 
+
+
+while True:
+    valor = int(input('digite 1 para conectar, 2 para pegar balanco, 3 mudar conta, 4 desconectar, 5 sair:'))
+    if valor==1:
+        email= input('email: ')
+        senha = input('senha: ')
+        print('Conectado: '+ str(api.conectar(email,senha)))
+        continue
+    elif valor==2:
+        print(api.pegar_balanco())
+        continue
+    elif valor==3:
+        conta=input('Usar conta:')
+        print('Mudou conta ==' + str(api.alterar_conta(conta.upper())))
+
+    elif valor == 4:
+        print('Deconetado ==' + str(api.desconectar()))      
+
+    else:
+        break    
+        
 
 
 
